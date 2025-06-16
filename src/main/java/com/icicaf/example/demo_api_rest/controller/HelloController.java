3ab6bb3471636a55037e3b4e643cacf38be04555
Aguayo Forteza, Cristian Alejandro <cristian.aguayo@icicaf.cl>
2025-06-15 21:22:31 -0400

    add sample log info

--- src/main/java/com/icicaf/example/demo_api_rest/controller/HelloController.java
+++ src/main/java/com/icicaf/example/demo_api_rest/controller/HelloController.java
@@ -4,11 +4,13 @@ import com.icicaf.example.demo_api_rest.dto.HelloDTO;
 import com.icicaf.example.demo_api_rest.dto.HelloRequest;
 import com.icicaf.example.demo_api_rest.service.HelloService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -20,6 +22,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public ResponseEntity<HelloDTO> sayHelloWorld(@RequestBody @Valid HelloRequest helloRequest) {
+        log.info("Log......");
         return ResponseEntity.ok(helloService.getHello(helloRequest.getName()));
     }
 
