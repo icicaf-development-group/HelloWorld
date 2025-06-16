07f82c7bb2e5a0e11a746159e93563998aa34188
Aguayo Forteza, Cristian Alejandro <cristian.aguayo@icicaf.cl>
2025-06-16 00:28:36 -0400

    add method get memory heap

--- src/main/java/com/icicaf/example/demo_api_rest/controller/HelloController.java
+++ src/main/java/com/icicaf/example/demo_api_rest/controller/HelloController.java
@@ -10,11 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @RestController
 public class HelloController {
 
     private final HelloService helloService;
+    private static final long BYTES_IN_MB = 1024 * 1024;
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
@@ -26,4 +33,21 @@ public class HelloController {
         return ResponseEntity.ok(helloService.getHello(helloRequest.getName()));
     }
 
+    @GetMapping("/api/memory/heap")
+    public Map<String, Object> getHeapMemory() {
+        MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage heapUsage = memoryBean.getHeapMemoryUsage();
+
+        Map<String, Object> heapInfo = new HashMap<>();
+        heapInfo.put("initMB", toMB(heapUsage.getInit()));
+        heapInfo.put("usedMB", toMB(heapUsage.getUsed()));
+        heapInfo.put("committedMB", toMB(heapUsage.getCommitted()));
+        heapInfo.put("maxMB", toMB(heapUsage.getMax()));
+
+        return heapInfo;
+    }
+
+    private long toMB(long bytes) {
+        return bytes / BYTES_IN_MB;
+    }
 }
